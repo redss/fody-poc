fb145a3dd0a14600c35d6692528c0283aa9022ef
redss <ryszard.tarajkowski@gmail.com>
2017-10-28 11:20:42 +0200

    Tested returning nulls.

---
+++ FodyPoc/ReturningNulls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using NullGuard;
+using NUnit.Framework;
+
+namespace FodyPoc
+{
+    [NullGuard(ValidationFlags.All)]
+    public class ReturningNulls
+    {
+        public string GetNull() => null;
+
+        [return: AllowNull]
+        public string AllowedGetNull() => null;
+    }
+
+    public class ReturningNullsTests
+    {
+        [Test]
+        public void cannot_get_null_from_null_protected_method()
+        {
+            Action gettingNull = () => new ReturningNulls().GetNull();
+
+            gettingNull.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Test]
+        public void can_get_null_when_it_is_explicitly_allowed()
+        {
+            Action gettingNull = () => new ReturningNulls().AllowedGetNull();
+
+            gettingNull.ShouldNotThrow();
+        }
+    }
+}
