aa6efba77ec1b10107d8bf4df4b111e787985e76
redss <ryszard.tarajkowski@gmail.com>
2017-10-28 11:12:02 +0200

    Checking if Fody's NullGuard works.

---
+++ FodyPoc/NullProtectedMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using NullGuard;
+using NUnit.Framework;
+
+namespace FodyPoc
+{
+    [NullGuard(ValidationFlags.All)]
+    public class NullProtectedMethod
+    {
+        public void Call(string notNullString)
+        {
+        }
+    }
+
+    public class NullProtectedMethodTests
+    {
+        [Test]
+        public void null_protected_method_can_be_called_with_actual_value()
+        {
+            CallingNullProtectedMethod("yo!").ShouldNotThrow();
+        }
+
+        [Test]
+        public void call_cannot_be_called_With_null()
+        {
+            CallingNullProtectedMethod(null).ShouldThrow<ArgumentNullException>();
+        }
+
+        private static Action CallingNullProtectedMethod(string argument)
+        {
+            return () => new NullProtectedMethod().Call(argument);
+        }
+    }
+}
